f5152d992e9b9900a903d6c98d39ea7f1bdff904
Sahil Verma <sahil.verma@vayana.com>
2025-02-27 22:06:50 +0530

    added socials

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,16 +6,16 @@ const Footer: React.FC = () => {
   return (
     <footer className="w-full p-4 text-m border-[#d47d6f] mt-auto">
       <div className="container mx-auto flex flex-wrap justify-center items-center">
-        <p>© {currentYear} VERSA •</p>
-        <Link href="https://mastodon.social/@yourusername" className="ml-2 hover:text-[#d47d6f] transition-colors duration-300">
+        <p>© {currentYear} Versa</p>
+        {/* <Link href="https://mastodon.social/@yourusername" className="ml-2 hover:text-[#d47d6f] transition-colors duration-300">
           Mastodon
-        </Link>
-        <span className="mx-1">•</span>
-        <Link href="https://twitter.com/yourusername" className="hover:text-[#d47d6f] transition-colors duration-300">
+        </Link> */}
+        <span className="mx-2">•</span>
+        <Link href="https://x.com/0xversa" className="hover:text-[#d47d6f] transition-colors duration-300">
           Twitter
         </Link>
-        <span className="mx-1">•</span>
-        <Link href="https://github.com/yourusername" className="hover:text-[#d47d6f] transition-colors duration-300">
+        <span className="mx-2">•</span>
+        <Link href="https://github.com/sahilvermadev" className="hover:text-[#d47d6f] transition-colors duration-300">
           GitHub
         </Link>
       </div>
