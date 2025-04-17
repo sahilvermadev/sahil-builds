e571ba1cdfb31711df6f8c0a3ecd863ebd3b9355
Sahil Verma <sahil.verma@vayana.com>
2025-04-17 17:49:30 +0530

    cred change

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,12 +6,12 @@ const Footer: React.FC = () => {
   return (
     <footer className="w-full p-4 text-m border-[#d47d6f] mt-auto">
       <div className="container mx-auto flex flex-wrap justify-center items-center">
-        <p>© {currentYear} Versa</p>
+        <p>© {currentYear} vrsa</p>
         {/* <Link href="https://mastodon.social/@yourusername" className="ml-2 hover:text-[#d47d6f] transition-colors duration-300">
           Mastodon
         </Link> */}
         <span className="mx-2">•</span>
-        <Link href="https://x.com/0xversa" className="hover:text-[#d47d6f] transition-colors duration-300">
+        <Link href="https://x.com/0xvrsa" className="hover:text-[#d47d6f] transition-colors duration-300">
           Twitter
         </Link>
         <span className="mx-2">•</span>
